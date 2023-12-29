699cf4608e8246891ce146e026281f7a458a8de7
Damien Metzler <111077+dmetzler@users.noreply.github.com>
2023-12-29 12:16:00 +0100

    Remove howto

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
       </div>
 
       <div className={styles.center}>
